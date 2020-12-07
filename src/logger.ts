bfa5698212bd4e73b81250ef6b3216bb2a674ad9
val <8445610+valmack@users.noreply.github.com>
2020-12-07 08:41:29 -0500

    feat: update file stream logger and add events and metrics loggers

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,10 @@
 import { Logger, LoggerModes } from '@overnightjs/logger'
+import { Writable } from 'stream'
+import * as logfmt from 'logfmt'
 import morgan from 'morgan'
 import { config } from 'node-config-ts'
 import * as rfs from 'rotating-file-stream'
+import { inherits } from 'util'
 
 enum LogLevel {
   debug = 1,
@@ -18,18 +21,24 @@ const logLevelMapping = {
 const LOG_LEVEL = config.logger.level && logLevelMapping[config.logger.level] || LogLevel.important
 const LOG_TO_FILES = config.logger.logToFiles || false
 const LOG_PATH = config.logger.filePath || '/usr/local/var/log/cas'
+const METRICS_FILE_PATH = LOG_PATH + '/metrics.log'
+const EVENTS_FILE_PATH = LOG_PATH + '/events.log'
+const REMOVE_TIMESTAMP = true
+
+// morgan combined
+// :remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"
+const LOG_FMT = '[:date[web]] ip=:remote-addr ts=:date[iso] method=:method path=:url http_version=:http-version req_header:req[header] status=:status content_length=:res[content-length] content_type=":res[content-type]" ref=:referrer user_agent=:user-agent elapsed_ms=:total-time[3]'
 
 /**
  * Handles logging
  */
-export class CASLogger {
+class CASLogger {
   public readonly logLevel: LogLevel
-  public readonly expressLoggers: any[]
   private consoleLogger: Logger
   private includeStackTrace: boolean
 
   constructor(logLevel: LogLevel) {
-    this.consoleLogger = new Logger(LoggerModes.Console, '', true)
+    this.consoleLogger = new Logger(LoggerModes.Console, '', REMOVE_TIMESTAMP)
     this.logLevel = logLevel
     this.includeStackTrace = this.logLevel == LogLevel.debug ? true : false
   }
@@ -41,14 +50,14 @@ export class CASLogger {
 
   public debug(content: string | object): void {
     if (this.logLevel > LogLevel.debug) {
-        return
+      return
     }
     this.consoleLogger.info(content, this.includeStackTrace)
   }
 
   public imp(content: string | object): void {
     if (this.logLevel > LogLevel.important) {
-        return
+      return
     }
     this.consoleLogger.imp(content, this.includeStackTrace)
   }
@@ -62,20 +71,110 @@ export class CASLogger {
   }
 }
 
-export const accessLogStream = rfs.createStream(`${LOG_PATH}/access.log`, {
-  size: "10M", // rotate every 10 MegaBytes written
-  interval: "1d", // rotate daily
-  compress: "gzip" // compress rotated files
-})
+class StreamLogger {
+  public blocked: boolean
+  private path: string
+  private stream: Writable
 
-export const logger = new CASLogger(LOG_LEVEL);
+  constructor(path: string, stream: Writable) {
+    this.path = path
+    this.stream = stream
+  }
 
+  protected writeStream(message: string): void {
+    if (this.blocked) {
+      console.warn(`Stream busy for ${this.path}. Some logs may be dropped.`)
+      return
+    }
+    this.blocked = true
+
+    this.stream.on('error', (err) => {
+      console.warn(err)
+      return
+    })
+    this.stream.on('drain', () => {
+      this.blocked = false
+      return
+    })
+    this.stream.on('finish', () => {
+      this.blocked = false
+      return
+    })
+    this.blocked = !this.stream.write(message + '\n', () => {
+      this.stream.end()
+      return
+    })
+  }
+}
+
+interface ServiceLog {
+  type: string
+  [key: string]: any
+}
+
+class ServiceLogger extends StreamLogger {
+  public service: string
+
+  constructor(service: string, path: string, stream: Writable) {
+    super(path, stream)
+    this.service = service
+  }
+
+  public log(content: ServiceLog, logToConsole?: boolean): void {
+    const message = `[${Date.now()}] service=${this.service} ${ServiceLogger.format(content)}`
+    this.writeStream(message)
+    if (LOG_LEVEL == LogLevel.debug) {
+      console.log(message)
+    } else if (logToConsole) {
+      console.log(message)
+    }
+  }
+
+  public static format(content: ServiceLog): string {
+    return logfmt.stringify(content)
+  }
+}
+
+export const logger = new CASLogger(LOG_LEVEL)
+
+export const expressLoggers = buildExpressMiddleware()
 function buildExpressMiddleware() {
   const middleware = [morgan('combined', { stream: logger })]
   if (LOG_TO_FILES) {
-    middleware.push(morgan('combined', { stream: accessLogStream }))
+    const accessLogStream = rfs.createStream(`${LOG_PATH}/access.log`, {
+      size: '10M',
+      interval: '1d',
+      compress: 'gzip'
+    })
+    middleware.push(morgan(LOG_FMT, { stream: accessLogStream }))
   }
   return middleware
 }
 
-export const expressLoggers = buildExpressMiddleware()
+const eventsStream = rfs.createStream(EVENTS_FILE_PATH, {
+    size: '10M',
+    interval: '1d',
+    compress: 'gzip'
+  })
+const metricsStream = rfs.createStream(METRICS_FILE_PATH, {
+    size: '10M',
+    interval: '1d',
+    compress: 'gzip'
+  })
+
+
+const dbEventsLogger = new ServiceLogger('db', EVENTS_FILE_PATH, eventsStream)
+const ethereumEventsLogger = new ServiceLogger('ethereum', EVENTS_FILE_PATH, eventsStream)
+
+const anchorMetricsLogger = new ServiceLogger('anchor', METRICS_FILE_PATH, metricsStream)
+const ethereumMetricsLogger = new ServiceLogger('ethereum', METRICS_FILE_PATH, metricsStream)
+
+export const logEvent = {
+  db: dbEventsLogger.log,
+  ethereum: ethereumEventsLogger.log
+}
+
+export const logMetric = {
+  anchor: anchorMetricsLogger.log,
+  ethereum: ethereumMetricsLogger.log
+}
