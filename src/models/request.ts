3b8b42761c44fd333d65ef03bf2f7e8f8923d6c2
val <8445610+valmack@users.noreply.github.com>
2020-12-10 21:19:08 -0500

    fix: move subscriber out of model

--- src/models/request.ts
+++ src/models/request.ts
@@ -1,18 +1,12 @@
 import { RequestStatus } from './request-status';
 import {
   Entity,
-  EntitySubscriberInterface,
-  EventSubscriber,
-  InsertEvent,
   PrimaryGeneratedColumn,
   Column,
   CreateDateColumn,
   UpdateDateColumn,
-  UpdateEvent,
   Unique
 } from 'typeorm';
-import { logEvent } from '../logger';
-import { setColumnsToUpdate, setUpdatedColumns } from './model-helpers';
 
 @Entity()
 @Unique(['cid'])
@@ -37,39 +31,6 @@ export class Request {
 
   @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
-}
-
-@EventSubscriber()
-export class RequestSubscriber implements EntitySubscriberInterface<Request> {
-  protected prevColumnsToUpdate: object
-  protected currUpdatedColumns: object
-  
-  listenTo(): typeof Request {
-    return Request;
-  }
-
-  afterInsert(event: InsertEvent<Request>): void {
-    logEvent.db({
-      type: 'request',
-      ...event.entity
-    })
-  }
-
-  beforeUpdate(event: UpdateEvent<Request>): Promise<any> | void {
-    setColumnsToUpdate(event, this.prevColumnsToUpdate)
-  }
-
-  afterUpdate(event: UpdateEvent<Request>): void {
-    setUpdatedColumns(event, this.currUpdatedColumns)
-
-    if ('status' in this.currUpdatedColumns) {
-      logEvent.db({
-        type: 'request',
-        status: this.currUpdatedColumns
-      });
-    }
-  }
-
 }
 
 /**
