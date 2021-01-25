33c45c22148eed8a338e1d2bf99d80d8ea1100d6
ukstv <sergey@ukstv.me>
2021-01-25 17:56:42 +0300

    feat: request creation is idempotent
    also present request in a uniform way

--- src/models/request-status.ts
+++ src/models/request-status.ts
@@ -4,3 +4,9 @@ export enum RequestStatus {
   COMPLETED = 2,
   FAILED = 3,
 }
+
+export class InvalidRequestStatusError extends Error {
+  constructor(status: never) {
+    super(`Invalid request status: ${status}`);
+  }
+}
