a4b231fe0cb0d427fd6672e08c8b1aac0c050720
Spencer T Brody <spencer@3box.io>
2021-01-29 10:43:59 -0500

    single quotes

--- src/controllers/anchor-controller.ts
+++ src/controllers/anchor-controller.ts
@@ -1,6 +1,6 @@
 import { INTERNAL_SERVER_ERROR, OK } from 'http-status-codes';
 import { Request as ExpReq, Response as ExpRes } from 'express';
-import { logger } from "../logger";
+import { logger } from '../logger';
 
 import cors from 'cors';
 import { Controller, Get, ClassMiddleware, Post } from "@overnightjs/core";
