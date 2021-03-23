6dd6f9e7ea12d2a7bc04a29e4c663b71e28b5f34
val <8445610+valmack@users.noreply.github.com>
2021-03-23 09:48:10 -0400

    feat: change ec to exit_code

--- runner.sh
+++ runner.sh
@@ -5,8 +5,8 @@ echo $TASKS
 
 if [[ $TASKS == "No running tasks found" ]]; then
   cd $CAS_PATH && npm run start
-  EC=$?
-  if [[ $EC != 0 ]]; then
+  exit_code=$?
+  if [[ $exit_code != 0 ]]; then
     echo "Service exited with non-zero exit code"
   fi
 fi
