f3b442660581f079ea2137c4b85484098c17a7bc
stickykeys <8445610+valmack@users.noreply.github.com>
2021-05-06 09:20:51 -0400

    feat: send discord notif if anchor hangs (#389)
    * feat: send discord notif if anchor hangs
    
    * feat: add cloudwatch logs url hanging notif
    
    * feat: rename cloudwatch var to base url

--- runner/check-aws-ecs-tasks.js
+++ runner/check-aws-ecs-tasks.js
@@ -1,3 +1,4 @@
+const https = require('https')
 const { ECSClient, ListTasksCommand } = require('@aws-sdk/client-ecs')
 
 async function main() {
@@ -26,11 +27,54 @@ async function main() {
       console.log('HALT')
       console.log('More than one task already running')
       console.log(data.taskArns)
+      const retryDelayMs = 300000 // 300k ms = 5 mins
+      sendNotification(data.taskArns, retryDelayMs)
     } else {
       console.log('OK')
       console.log('Only one running task found (assumed to be self)')
     }
   }
+}
+
+function sendNotification(taskArns, retryDelayMs = -1) {
+  const url = process.env.DISCORD_WEBHOOK_URL
+  const options = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
+  const arnRegex = /\w+$/
+  const fields = data.taskArns.map((arn, index) => {
+    let value = arn
+    const id = arn.match(arnRegex)
+    if (id) {
+      value = `${process.env.CLOUDWATCH_LOG_BASE_URL}/${id[0]}`
+    }
+    return { name: `Task ${index}`, value }
+  })
+  const message = [
+    {
+      title: 'CAS still running',
+      description: `A new CAS anchor task was not started because there is already at least one running.`,
+      color: 16776960,
+      fields,
+    },
+  ]
+  const data = { embeds: message, username: 'cas-runner' }
+
+  const req = https.request(url, options, (res) => {
+    console.log(`Notification request status code: ${res.statusCode}`)
+    if (res.statusCode >= 500 && retryDelayMs > -1) {
+      console.log(`Retrying after ${retryDelayMs} milliseconds...`)
+      setTimeout(() => {
+        sendNotification(taskArns)
+      }, retryDelayMs)
+    }
+  })
+  req.on('error', console.error)
+  req.write(JSON.stringify(data))
+  req.end()
 }
 
 main()
