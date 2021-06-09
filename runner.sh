46d561bffbd378ca5debb375bb6dd3743d60eeee
stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2021-06-09 13:03:06 -0400

    fix(runner): run tasks in root dir

--- runner.sh
+++ runner.sh
@@ -8,7 +8,7 @@ if [[ ${STATUS[0]} == "OK" ]]; then
   cd $CAS_PATH && npm run start
   exit_code=$?
   if [[ $exit_code != 0 ]]; then
-    node ./runner/report-exit.js
+    cd $HOME && node ./runner/report-exit.js
     echo "CAS exited with non-zero exit code"
   fi
 fi
