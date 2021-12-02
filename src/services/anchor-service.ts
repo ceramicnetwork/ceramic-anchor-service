5df338935cf72eba3ecd5988ed5fc058ebc9b5d4
Spencer T Brody <spencer+github@3box.io>
2021-12-02 16:22:44 -0500

    fix: Do not create anchor commits in parallel

--- src/services/anchor-service.ts
+++ src/services/anchor-service.ts
@@ -258,12 +258,17 @@ export default class AnchorService {
     merkleTree: MerkleTree<CIDHolder, Candidate, TreeMetadata>
   ): Promise<Anchor[]> {
     const candidates = merkleTree.getLeaves()
-    const anchors = await Promise.all(
-      Array.from(candidates.entries()).map(([index, candidate]) =>
-        this._createAnchorCommit(candidate, index, ipfsProofCid, merkleTree)
-      )
-    )
-    return anchors.filter((anchor) => anchor != null)
+    const anchors = []
+
+    for (let i = 0; i < candidates.length; i++) {
+      const candidate = candidates[i]
+      const anchor = await this._createAnchorCommit(candidate, i, ipfsProofCid, merkleTree)
+      if (anchor) {
+        anchors.push(anchor)
+      }
+    }
+
+    return anchors
   }
 
   /**
