30d57edc4378f09decb368e363ebca9351de1391
stephhuynh18 <stephhuynh18@users.noreply.github.com>
2022-06-07 13:43:48 -0700

    Remove cron, PR comments about testing

--- src/services/__tests__/scheduler-service.test.ts
+++ src/services/__tests__/scheduler-service.test.ts
@@ -15,8 +15,16 @@ describe('scheduler service', () => {
     const numberOfRunsBeforeDone = 3
     const schedulerService = container.resolve<SchedulerService>('schedulerService')
 
+    const task = jest.fn()
+
+    const runChecks = () => {
+      // the task runs once right at the start before running every X seconds
+      expect(task.mock.calls.length).toEqual(numberOfRunsBeforeDone + 1)
+      done()
+    }
+
     let count = 0
-    const task = jest.fn(() => {
+    task.mockImplementation(() => {
       if (count === numberOfRunsBeforeDone) {
         schedulerService.stop()
         runChecks()
@@ -26,20 +34,23 @@ describe('scheduler service', () => {
       return Promise.resolve()
     })
 
-    schedulerService.start(task)
-    const runChecks = () => {
-      // the task runs once right at the start before running every X seconds
-      expect(task.mock.calls.length).toEqual(numberOfRunsBeforeDone + 1)
-      done()
-    }
+    schedulerService.start(task as any)
+    // test doesn't complete until 'done()' is called
   })
 
   test('will continue if the task fails', (done) => {
     const numberOfRunsBeforeDone = 5
     const schedulerService = container.resolve<SchedulerService>('schedulerService')
 
+    const task = jest.fn()
+    const runChecks = () => {
+      // the task runs once right at the start before running every X seconds
+      expect(task.mock.calls.length).toEqual(numberOfRunsBeforeDone + 1)
+      done()
+    }
+
     let count = 0
-    const task = jest.fn(() => {
+    task.mockImplementation(() => {
       if (count === numberOfRunsBeforeDone) {
         schedulerService.stop()
         runChecks()
@@ -47,18 +58,15 @@ describe('scheduler service', () => {
 
       count = count + 1
 
-      if (count > 3) {
+      // the last two runs will be rejected
+      if (count > numberOfRunsBeforeDone - 2) {
         return Promise.reject('test error')
       }
 
       return Promise.resolve()
     })
 
-    schedulerService.start(task)
-    const runChecks = () => {
-      // the task runs once right at the start before running every X seconds
-      expect(task.mock.calls.length).toEqual(numberOfRunsBeforeDone + 1)
-      done()
-    }
+    schedulerService.start(task as any)
+    // test doesn't complete until 'done()' is called
   })
 })
