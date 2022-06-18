8ecde9b100f0288722f1a2bfd9f5ba8e922c59b0
0xJovonni <4885245+Jovonni@users.noreply.github.com>
2022-06-18 11:32:48 -0400

    fixed dependency installation using forge

--- contracts/test/CeramicAnchorServiceV2.t.sol
+++ contracts/test/CeramicAnchorServiceV2.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import "forge-std/src/Test.sol";
 import "../src/CeramicAnchorServiceV2.sol";
 
 contract CeramicAnchorServiceV2Test is Test {
