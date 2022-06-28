ab912054bf44d00592368b7e84e388069cf0cc8f
stephhuynh18 <stephhuynh18@users.noreply.github.com>
2022-06-28 08:29:52 -0700

    Wait for anchor in test

--- src/__tests__/ceramic_integration.test.ts
+++ src/__tests__/ceramic_integration.test.ts
@@ -429,7 +429,7 @@ describe('Ceramic Integration Test', () => {
       60 * 1000 * 2
     )
 
-    test('Anchor discovered through pubsub', async () => {
+    test.only('Anchor discovered through pubsub', async () => {
       jest.setTimeout(60 * 1000 * 2)
       // In ceramic the stream waits for a successful anchor by polling the request endpoint of the CAS.
       // We alter the CAS' returned request anchor status so that it is always pending.
@@ -465,6 +465,7 @@ describe('Ceramic Integration Test', () => {
         expect(doc1.state.anchorStatus).toEqual(AnchorStatus.ANCHORED)
         expect(doc1.content).toEqual(updatedContent)
 
+        await waitForAnchor(doc2)
         await doc2.sync({ sync: SyncOptions.NEVER_SYNC })
         expect(doc2.state.anchorStatus).toEqual(AnchorStatus.ANCHORED)
         expect(doc2.content).toEqual(updatedContent)
