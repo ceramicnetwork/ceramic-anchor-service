797abbee5a6b73ca2b87ffa839d4f4cb55e7e954
stephhuynh18 <stephhuynh18@users.noreply.github.com>
2022-07-03 19:28:39 -0700

    Add todo to use localstack

--- src/services/__tests__/event-producer-service.test.ts
+++ src/services/__tests__/event-producer-service.test.ts
@@ -5,6 +5,8 @@ import { config } from 'node-config-ts'
 import { EventProducerService } from '../event-producer/event-producer-service.js'
 import { SQSEventProducerService } from '../event-producer/sqs/sqs-event-producer-service.js'
 
+// TODO: Use local stack instead of an actual AWS SQS Queue
+// https://linear.app/3boxlabs/issue/NET-1633/use-localstack-instead-of-an-aws-sqs-queue-in-cas-tests
 describe('event service', () => {
   beforeAll(async () => {
     container.registerInstance('config', config)
