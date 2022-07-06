3accda9cfb3086e00e002f714aceda1b8b7cb8d6
Golda Velez <golda@3box.io>
2022-07-06 15:38:23 -0700

    add settings

---
+++ src/settings.ts
@@ -0,0 +1,10 @@
+
+
+export const METRIC_NAMES = {
+  ANCHOR_SUCCESS: 'anchor_success',
+  ANCHOR_TIMEOUT: 'anchor_timeout',
+  ERROR_ETH: 'error_eth',
+  RETRY_EMIT_ANCHOR_EVENT: 'retry_emit_anchor_event'
+}
+
+
