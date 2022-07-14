7f89f0782987bbc22ac806e5b1ef30b80bcf2528
0xJovonni <4885245+Jovonni@users.noreply.github.com>
2022-07-14 10:28:33 -0400

    added contract anchoring to integration tests

--- src/__tests__/ceramic_integration.test.ts
+++ src/__tests__/ceramic_integration.test.ts
@@ -120,7 +120,6 @@ async function makeGanache(startTime: Date, port: number): Promise<Ganache.Serve
 
   const options = {};
 
-  // var privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
   const privateKey = privKey
   var wallet = new ethers.Wallet(privateKey);
   let p = new ethers.providers.JsonRpcProvider(`http://localhost:${port}`);
