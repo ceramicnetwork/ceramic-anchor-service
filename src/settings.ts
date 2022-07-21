7e69c149ad385ff6cf77320f0b2e935fe553c5ba
Golda Velez <golda@3box.io>
2022-07-21 16:11:58 +0200

    fix: use enums for controlled vocab

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,6 @@
-export const METRIC_NAMES = {
-  ANCHOR_SUCCESS: 'anchor_success',
-  ANCHOR_TIMEOUT: 'anchor_timeout',
-  ERROR_ETH: 'error_eth',
-  RETRY_EMIT_ANCHOR_EVENT: 'retry_emit_anchor_event',
+export enum METRIC_NAMES {
+  ANCHOR_SUCCESS = 'anchor_success',
+  ANCHOR_TIMEOUT = 'anchor_timeout',
+  ERROR_ETH = 'error_eth',
+  RETRY_EMIT_ANCHOR_EVENT = 'retry_emit_anchor_event'
 }
