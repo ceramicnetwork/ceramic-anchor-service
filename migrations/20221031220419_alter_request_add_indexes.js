de20aba574f54084ee2953985f3b15895b04c328
Stephanie Huynh <stephhuynh18@users.noreply.github.com>
2022-11-02 14:07:31 -0700

    feat: Add indexes (#803)
    * Add indexes
    
    * Add index for anchor cid remove status index

---
+++ migrations/20221031220419_alter_request_add_indexes.js
@@ -0,0 +1,38 @@
+const REQUEST_TABLE_NAME = 'request'
+const ANCHOR_TABLE_NAME = 'anchor'
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.alterTable(REQUEST_TABLE_NAME, (table) => {
+    table.index(['stream_id'])
+    table.index(['created_at'])
+    table.index(['updated_at'])
+    table.index(['status', 'created_at'])
+    table.index(['status', 'updated_at'])
+  })
+
+  await knex.schema.alterTable(ANCHOR_TABLE_NAME, (table) => {
+    table.index(['cid'])
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.schema.alterTable(REQUEST_TABLE_NAME, (table) => {
+    table.dropIndex(['stream_id'])
+    table.dropIndex(['created_at'])
+    table.dropIndex(['updated_at'])
+    table.dropIndex(['status', 'created_at'])
+    table.dropIndex(['status', 'updated_at'])
+  })
+
+  await knex.schema.alterTable(ANCHOR_TABLE_NAME, (table) => {
+    table.dropIndex(['cid'])
+  })
+}
