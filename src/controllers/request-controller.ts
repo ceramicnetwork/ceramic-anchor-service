154b3bf83e240a30b1e43030c2ba5b3f9d9fae99
Sergey Ukustov <sergey@ukstv.me>
2022-11-30 18:30:55 +0300

    feat: Add timestamp to request

--- src/controllers/request-controller.ts
+++ src/controllers/request-controller.ts
@@ -15,6 +15,7 @@ import { ServiceMetrics as Metrics } from '../service-metrics.js'
 import { METRIC_NAMES } from '../settings.js'
 import { RequestPresentation } from '../models/request-presentation.js'
 import { CeramicService } from '../services/ceramic-service.js'
+import { StreamID } from '@ceramicnetwork/streamid'
 
 @Controller('api/v0/requests')
 @ClassMiddleware([cors()])
@@ -70,49 +71,48 @@ export class RequestController {
     try {
       logger.debug(`Create request ${JSON.stringify(req.body)}`)
 
-      const cid = req.body.cid
-      // TODO docId check for backwards compat with old Ceramic nodes. Remove once
-      //  https://github.com/ceramicnetwork/js-ceramic/issues/1485 has been merged, released, and
-      // deployed to a majority of the network.
-      const streamId = req.body.streamId || req.body.docId
-
-      if (cid == null) {
+      if (!req.body.cid) {
         return res.status(StatusCodes.BAD_REQUEST).send({
           error: 'CID is empty',
         })
       }
-
-      if (streamId == null) {
+      const cid = toCID(req.body.cid)
+      if (!req.body.streamId) {
         return res.status(StatusCodes.BAD_REQUEST).send({
           error: 'Stream ID is empty',
         })
       }
+      const streamId = StreamID.fromString(req.body.streamId)
 
-      const cidObj = toCID(cid)
-      let request = await this.requestRepository.findByCid(cidObj)
+      let timestamp = new Date()
+      if (req.body.timestamp) {
+        timestamp = new Date(req.body.timestamp)
+      }
+
+      let request = await this.requestRepository.findByCid(cid)
       if (request) {
         const body = await this.#requestPresentation.body(request)
         return res.status(StatusCodes.ACCEPTED).json(body)
-      } else {
-        // Intentionally don't await the pinStream promise, let it happen in the background.
-        this.ceramicService.pinStream(streamId)
-        Metrics.count(METRIC_NAMES.ANCHOR_REQUESTED, 1, { ip_addr: req.ip })
-
-        request = new Request()
-        request.cid = cid.toString()
-        request.streamId = streamId
-        request.status = RequestStatus.PENDING
-        request.message = 'Request is pending.'
-        // We don't actually know with certainty that the stream is pinned, since the pinStream
-        // call above can fail and swallows errors, but marking it as pinned incorrectly is harmless,
-        // and this way we ensure the request is picked up by garbage collection.
-        request.pinned = true
-
-        request = await this.requestRepository.createOrUpdate(request)
-
-        const body = await this.#requestPresentation.body(request)
-        return res.status(StatusCodes.CREATED).json(body)
       }
+      // Intentionally don't await the pinStream promise, let it happen in the background.
+      this.ceramicService.pinStream(streamId)
+      Metrics.count(METRIC_NAMES.ANCHOR_REQUESTED, 1, { ip_addr: req.ip })
+
+      request = new Request()
+      request.cid = cid.toString()
+      request.streamId = streamId.toString()
+      request.status = RequestStatus.PENDING
+      request.message = 'Request is pending.'
+      // We don't actually know with certainty that the stream is pinned, since the pinStream
+      // call above can fail and swallows errors, but marking it as pinned incorrectly is harmless,
+      // and this way we ensure the request is picked up by garbage collection.
+      request.pinned = true
+      request.timestamp = timestamp
+
+      request = await this.requestRepository.createOrUpdate(request)
+
+      const body = await this.#requestPresentation.body(request)
+      return res.status(StatusCodes.CREATED).json(body)
     } catch (err) {
       const errmsg = `Creating request with streamId ${req.body.streamId} and commit CID ${req.body.cid} failed: ${err.message}`
       logger.err(errmsg)
