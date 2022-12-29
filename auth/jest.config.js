7a3e1a404f376c7c8c832406864a027befa85162
v-stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2022-12-29 09:26:14 -0500

    test: add create email verification test

---
+++ auth/jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
+    "uuid": require.resolve('uuid'),
+  }
+};
---
+++ auth/src/__tests__/createEmailVerification.ts
@@ -0,0 +1,65 @@
+import { createEmailVerificationCode, DatabaseEmailVerification } from '../utils/db'
+
+test('createEmailVerification', async () => {
+    const activeOTPs: any[] = []
+    const revokedOTPs: any[] = []
+
+    interface TestDb extends DatabaseEmailVerification {
+      createEmailVerificationCode: any
+    }
+
+    let db: TestDb = {
+      createEmailVerificationCode: async function (email: string) {
+        await createEmailVerificationCode(email, db)
+      },
+      _getActiveOTPs: jest.fn(() => {
+        return Promise.resolve(activeOTPs)
+      }),
+      _getRevokedOTPs: jest.fn(() => {
+        return Promise.resolve(revokedOTPs)
+      }),
+      _checkOTPExpired: jest.fn(),
+      _expireOTP: jest.fn(() => {
+        revokedOTPs.shift()
+        return Promise.resolve()
+      }),
+      _revokeOTP: jest.fn((item) => {
+        activeOTPs.shift()
+        revokedOTPs.push(item.otp)
+        return Promise.resolve()
+      }),
+      _addOTP: jest.fn((email, otp) => {
+        activeOTPs.push({otp})
+        return Promise.resolve()
+      })
+    }
+    jest.spyOn(db, '_checkOTPExpired')
+      .mockImplementationOnce(() => {
+        return false
+      })
+      .mockImplementationOnce(() => {
+        return true
+      })
+
+    const email = 'memail@wemail.bemail'
+
+    // 1
+    await db.createEmailVerificationCode(email)
+    expect(db._addOTP).toHaveBeenCalledTimes(1)
+    // 2
+    await db.createEmailVerificationCode(email)
+    expect(db._revokeOTP).toHaveBeenCalledTimes(1)
+    expect(db._addOTP).toHaveBeenCalledTimes(2)
+    // 3
+    await expect(db.createEmailVerificationCode(email)).rejects.toThrow('Must wait until existing codes expire')
+    expect(db._expireOTP).toHaveBeenCalledTimes(0)
+    expect(db._addOTP).toHaveBeenCalledTimes(2)
+    // 4
+    await expect(db.createEmailVerificationCode(email)).rejects.toThrow('Must wait until existing codes expire')
+    expect(db._expireOTP).toHaveBeenCalledTimes(1)
+    expect(db._addOTP).toHaveBeenCalledTimes(2)
+    // 5
+    await db.createEmailVerificationCode(email)
+    expect(db._revokeOTP).toHaveBeenCalledTimes(2)
+    expect(db._addOTP).toHaveBeenCalledTimes(3)
+})
