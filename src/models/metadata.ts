47addf93ea616e6789a33d574ce98a8c90ff1a95
Sergey Ukustov <sergey@ukstv.me>
2022-12-30 19:20:10 +0300

    chore: Name codecs

--- src/models/metadata.ts
+++ src/models/metadata.ts
@@ -23,29 +23,41 @@ export type GenesisFields = t.TypeOf<typeof GenesisFields>
 /**
  * Metadata entry that we are about to store to a database.
  */
-export const FreshMetadata = t.type({
-  streamId: t.string.pipe(te.streamIdAsString),
-  metadata: GenesisFields,
-})
+export const FreshMetadata = t.type(
+  {
+    streamId: t.string.pipe(te.streamIdAsString),
+    metadata: GenesisFields,
+  },
+  'FreshMetadata'
+)
 export type FreshMetadata = t.TypeOf<typeof FreshMetadata>
 
 /**
  * Fields populated from a database.
  */
-export const StoredMetadataFields = t.type({
-  createdAt: te.date,
-  updatedAt: te.date,
-  usedAt: te.date,
-})
+export const StoredMetadataFields = t.type(
+  {
+    createdAt: te.date,
+    updatedAt: te.date,
+    usedAt: te.date,
+  },
+  'StoredMetadataFields'
+)
 
 /**
  * Full metadata entry if retrieved from a database.
  */
-export const StoredMetadata = t.intersection([FreshMetadata, StoredMetadataFields])
+export const StoredMetadata = t.intersection(
+  [FreshMetadata, StoredMetadataFields],
+  'StoredMetadata'
+)
 export type StoredMetadata = t.TypeOf<typeof StoredMetadata>
 
 /**
  * Metadata entry that can be stored to a database.
  */
-export const MetadataInput = t.union([FreshMetadata, t.partial(StoredMetadataFields.props)])
+export const MetadataInput = t.union(
+  [FreshMetadata, t.partial(StoredMetadataFields.props)],
+  'MetadataInput'
+)
 export type MetadataInput = t.TypeOf<typeof MetadataInput>
