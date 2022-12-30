817dd867f440e55ead640263c4f2b29312b212ea
v-stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2022-12-30 15:05:38 -0500

    test: add integration tests for api

---
+++ auth/src/__tests__/integration.spec.ts
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch'
+import { httpMethods } from "../utils/reqres"
+
+const endpoint = (path?: string) => `http://localhost:3000/dev/api/v0/auth${path || ''}`
+describe('api', () => {
+    test.skip('verify email', async () => {
+        const resp = await fetch(endpoint('/verification'), {
+            method: httpMethods.POST,
+            body: JSON.stringify({email: 'val@3box.io'})
+        })
+        console.log(resp.status)
+        console.log(resp.body)
+    })
+
+    test.skip('register did', async () => {
+        const resp = await fetch(endpoint('/did'), {
+            method: httpMethods.POST,
+            body: JSON.stringify({
+                email: 'val@3box.io',
+                dids: ['did:key:z6MktCFRcwLRFQA9WbeDRM7W7kbBdZTHQ2xnPgyxZLq1gCpK'],
+                otp: '4a0806fe-de9d-49cb-9ed5-64b7d4481a03'
+            })
+        })
+        console.log(resp.status)
+        console.log(await resp.json())
+    })
+})
