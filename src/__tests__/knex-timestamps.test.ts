e08d8c7bd0b149ccdeee177e9c0a36dd3b9b76d3
Sergey Ukustov <sergey@ukstv.me>
2023-01-03 20:17:17 +0300

    test: JS-to-PG conversion ignores timezone

--- src/__tests__/knex-timestamps.test.ts
+++ src/__tests__/knex-timestamps.test.ts
@@ -27,32 +27,24 @@ test('PG _now_ corresponds to JS _now_', async () => {
 test('JS-to-PG conversion ignores timezone', async () => {
   // thus manual conversion to ISO8601 is required
   const timestampWithoutTimezone = '2023-01-02T03:04:05.678'
-  const withTimezone = new Date(`${timestampWithoutTimezone}+08:00`) // In Singapore time zone
+  const timestampSGP = `${timestampWithoutTimezone}+08:00`
+  const withTimezone = new Date(timestampSGP) // In Singapore time zone
   const pgTimestamp = await dbConnection
     .table('metadata')
     .insert({ streamId: randomStreamID().toString(), metadata: {}, usedAt: withTimezone })
     .returning('usedAt')
     .then((rows) => rows[0].usedAt as Date)
-  console.log('pg.a', timestampWithoutTimezone, withTimezone)
-  console.log('pg.0', pgTimestamp)
+  const timezoneOffsetMinutes = new Date().getTimezoneOffset() // minutes
+  const timezoneOffsetMs = timezoneOffsetMinutes * 60 * 1000 // milliseconds
+  expect(pgTimestamp.valueOf()).toEqual(withTimezone.valueOf() - timezoneOffsetMs)
   const pgTimestamp1 = await dbConnection
     .table('metadata')
-    .insert({ streamId: randomStreamID().toString(), metadata: {}, usedAt: withTimezone.toISOString() })
+    .insert({
+      streamId: randomStreamID().toString(),
+      metadata: {},
+      usedAt: withTimezone.toISOString(),
+    })
     .returning('usedAt')
     .then((rows) => rows[0].usedAt as Date)
-  console.log('pg.1', pgTimestamp1)
-  // expect(pgTimestamp.toISOString()).toEqual(`${timestampWithoutTimezone}Z`)
-})
-
-test('foo', async () => {
-  const now0 = new Date()
-  // const now = new Date()
-  // const nowZ = now.toISOString()
-  // console.log('nowZ', nowZ)
-  // const nowSGP = nowZ.replace(/Z$/, '+08:00')
-  // console.log('nowSGP', new Date(nowSGP))
-  // console.log('now', now)
-  // // await dbConnection.table('metadata').insert({ streamId: 'foo', metadata: {}, usedAt: now })
-  // // const all = await dbConnection.table('metadata')
-  // // console.log('all', all)
+  expect(pgTimestamp1.toISOString()).toEqual('2023-01-01T19:04:05.678Z')
 })
