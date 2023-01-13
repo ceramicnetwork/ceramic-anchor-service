c1ba74fa5b628d22576c619f2f51f210cd6fa0d6
Sergey Ukustov <sergey@ukstv.me>
2023-01-13 16:36:37 +0300

    test: Fix segfault by using CJS migrations

--- migrations/20221128160000_add_metadata.cjs
+++ migrations/20221128160000_add_metadata.cjs
@@ -6,7 +6,7 @@ const TABLE_NAME = 'metadata'
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-export async function up(knex) {
+async function up(knex) {
   await knex.schema.createTable(TABLE_NAME, (table) => {
     table.string('stream_id', 1024).notNullable().unique().primary().index()
     table.timestamp('created_at', { useTz: false }).notNullable().defaultTo(knex.raw('now()'))
@@ -20,6 +20,8 @@ export async function up(knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-export async function down(knex) {
+async function down(knex) {
   await knex.schema.dropTable(TABLE_NAME)
 }
+
+module.exports = { up, down }
