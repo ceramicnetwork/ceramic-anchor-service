1ba49c93ca0182d6dfd17c4fd689af50325ea0cb
Sergey Ukustov <sergey@ukstv.me>
2023-01-18 17:24:14 +0300

    feat: Add tables to the database (#846)

---
+++ migrations/20221128162000_alter_request_add_origin.js
@@ -0,0 +1,22 @@
+const REQUEST_TABLE_NAME = 'request'
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.alterTable(REQUEST_TABLE_NAME, (table) => {
+    table.string('origin', 1024).index()
+    table.timestamp('timestamp', { useTz: false }).notNullable().defaultTo(knex.raw('now()'))
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.schema.alterTable(REQUEST_TABLE_NAME, (table) => {
+    table.dropColumns('origin', 'timestamp')
+  })
+}
