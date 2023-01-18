35b1a620dd7cdaf07124f0027b71eb2a8b41bbef
v-stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2023-01-18 07:25:56 -0500

    fix: add iam utils

---
+++ auth/src/utils/iam.ts
@@ -0,0 +1,27 @@
+export function generatePolicy(principalId: string, policyDocumentOptions?: { effect: string, resource: string}, usageIdentifierKey?: string) {
+    const authResponse: any = {
+        principalId,
+    }
+
+    if (policyDocumentOptions) {
+        const policyDocument = {
+        Version: '2012-10-17',
+        Statement: [
+        {
+            Action: 'exevute-api:Invoke',
+            Effect: policyDocumentOptions.effect,
+            Resource: policyDocumentOptions.resource
+        }]}
+        authResponse.policyDocument = policyDocument
+    }
+
+    if (usageIdentifierKey) {
+        authResponse.usageIdentifierKey = usageIdentifierKey
+        authResponse.context = {
+            authId: usageIdentifierKey
+        }
+    }
+    // Add custom properties on context if needed
+    // authResponse.context
+    return authResponse
+}
