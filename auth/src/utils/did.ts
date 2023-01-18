62b7abfeacee8fbbb267b22cc04445e29f188789
v-stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2023-01-18 07:34:33 -0500

    feat: add createRandomDID util

--- auth/src/utils/did.ts
+++ auth/src/utils/did.ts
@@ -1,5 +1,9 @@
 import { DID, VerifyJWSResult } from 'dids'
-import KeyResolver from 'key-did-resolver'
+import KeyDIDResolver from 'key-did-resolver'
+import { Ed25519Provider } from "key-did-provider-ed25519"
+import * as u8a from 'uint8arrays'
+import { Resolver } from 'did-resolver'
+import { randomBytes } from '@stablelib/random'
 
 export const didRegex = new RegExp(/\S+:\S+:\S+/)
 
@@ -27,6 +31,12 @@ export async function checkValidSignature(did: string, jws: string): Promise<boo
 
 export async function parseSignature(jws: string): Promise<VerifyJWSResult> {
     // @ts-ignore
-    const verifier = new DID({ resolver: KeyResolver.getResolver() })
+    const verifier = new DID({ resolver: KeyDIDResolver.getResolver() })
     return await verifier.verifyJWS(jws)
+}
+
+export function createRandomDID(): DID {
+    const seed = randomBytes(32)
+    // @ts-ignore
+    return new DID({ provider: new Ed25519Provider(seed), resolver: KeyDIDResolver.getResolver() })
 }
