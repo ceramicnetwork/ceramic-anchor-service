dcbcbf0ca96d815fd13d42345cd3b3eaaf5b01b3
Sergey Ukustov <sergey@ukstv.me>
2023-01-19 21:30:44 +0300

    chore: Ancillary io-ts codecs necessary for CAS w/o Node project (#896)
    * chore: Add io-ts, fp-ts, conditional-type-checks deps
    
    * chore: Ancillary io-ts codecs we use (or going to use)

---
+++ src/ancillary/did-string.ts
@@ -0,0 +1,13 @@
+import type { Opaque } from './opaque.type.js'
+
+export type DIDString = Opaque<string, 'DIDString'>
+
+const PCT_ENCODED = '(?:%[0-9a-fA-F]{2})'
+const ID_CHAR = `(?:[a-zA-Z0-9._-]|${PCT_ENCODED})`
+const METHOD = '([a-z0-9]+)'
+const METHOD_ID = `((?:${ID_CHAR}*:)*(${ID_CHAR}+))`
+const DID_MATCHER = new RegExp(`^did:${METHOD}:${METHOD_ID}$`)
+
+export function isDIDString(input: string): input is DIDString {
+  return Boolean(input && input.match(DID_MATCHER))
+}
