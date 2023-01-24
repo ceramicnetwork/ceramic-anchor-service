82d06a7f6969ca305df4070d6b3f1ba15083d3a5
Sergey Ukustov <sergey@ukstv.me>
2023-01-24 19:46:10 +0300

    chore: Drop dead code

--- src/utils.ts
+++ src/utils.ts
@@ -41,16 +41,4 @@ export class Utils {
     const cidVersion = 1
     return CID.create(cidVersion, ETH_TX_CODE, multihash)
   }
-
-  /**
-   * Average array of integers
-   * @param arr - Array of number
-   */
-  static averageArray(arr: number[]): number {
-    if (arr.length == 0) {
-      return 0
-    }
-
-    return arr.reduce((total, aNumber) => total + aNumber, 0) / arr.length
-  }
 }
