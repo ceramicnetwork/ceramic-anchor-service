aac82511ded07feefe703ea4580b38db6a5964c5
Sergey Ukustov <sergey@ukstv.me>
2023-01-24 15:23:05 +0300

    feat: Store metadata from genesis to the database

---
+++ src/controllers/__tests__/request-controller.test.ts
@@ -0,0 +1,216 @@
+import { describe, expect, jest, test } from '@jest/globals'
+import { createDbConnection } from '../../db-connection.js'
+import { createInjector, Injector } from 'typed-inject'
+import { config } from 'node-config-ts'
+import { RequestController } from '../request-controller.js'
+import { RequestPresentationService } from '../../services/request-presentation-service.js'
+import { RequestRepository } from '../../repositories/request-repository.js'
+import { StatusCodes } from 'http-status-codes'
+import {
+  MockCeramicService,
+  MockIpfsService,
+  randomCID,
+  randomStreamID,
+} from '../../__tests__/test-utils.js'
+import type { Knex } from 'knex'
+import { RequestStatus } from '../../models/request.js'
+import type { IIpfsService } from '../../services/ipfs-service.type.js'
+import type { StreamID } from '@ceramicnetwork/streamid'
+import type { IMetadataService } from '../../services/metadata-service.js'
+import { mockRequest, mockResponse } from './mock-request.util.js'
+
+type Tokens = {
+  requestController: RequestController
+  requestRepository: RequestRepository
+  ipfsService: IIpfsService
+  metadataService: IMetadataService
+}
+
+class MockMetadataService implements IMetadataService {
+  async fill(streamId: StreamID): Promise<void> {
+    return
+  }
+}
+
+describe('createRequest', () => {
+  let dbConnection: Knex
+  let container: Injector<Tokens>
+  let controller: RequestController
+
+  beforeAll(async () => {
+    dbConnection = await createDbConnection()
+    container = createInjector()
+      .provideValue('config', config)
+      .provideValue('dbConnection', dbConnection)
+      .provideClass('requestRepository', RequestRepository)
+      .provideClass('anchorRepository', RequestRepository)
+      .provideClass('ipfsService', MockIpfsService)
+      .provideClass('ceramicService', MockCeramicService)
+      .provideClass('requestPresentationService', RequestPresentationService)
+      .provideClass('metadataService', MockMetadataService)
+      .provideClass('requestController', RequestController)
+    controller = container.resolve('requestController')
+  })
+
+  afterAll(async () => {
+    await dbConnection.destroy()
+  })
+
+  describe('fresh request', () => {
+    test('cid is empty', async () => {
+      const req = mockRequest()
+      const res = mockResponse()
+      await controller.createRequest(req, res)
+      expect(res.status).toBeCalledWith(StatusCodes.BAD_REQUEST)
+      expect(res.json).toBeCalledWith({
+        error: 'CID is empty',
+      })
+    })
+    test('streamId is empty', async () => {
+      const req = mockRequest({
+        body: {
+          cid: randomCID().toString(),
+        },
+      })
+      const res = mockResponse()
+      await controller.createRequest(req, res)
+      expect(res.status).toBeCalledWith(StatusCodes.BAD_REQUEST)
+      expect(res.json).toBeCalledWith({
+        error: 'Stream ID is empty',
+      })
+    })
+    test('cid is malformed', async () => {
+      const streamId = randomStreamID()
+      const req = mockRequest({
+        body: {
+          cid: 'garbage',
+          streamId: streamId.toString(),
+        },
+      })
+      const res = mockResponse()
+      await controller.createRequest(req, res)
+      expect(res.status).toBeCalledWith(StatusCodes.BAD_REQUEST)
+      const jsonFn = jest.spyOn(res, 'json')
+      expect(jsonFn.mock.calls[0][0].error).toMatch(
+        `Creating request with streamId ${req.body.streamId} and commit CID ${req.body.cid} failed`
+      )
+    })
+    test('streamId is malformed', async () => {
+      const req = mockRequest({
+        body: {
+          cid: randomCID().toString(),
+          streamId: 'garbage',
+        },
+      })
+      const res = mockResponse()
+      await controller.createRequest(req, res)
+      expect(res.status).toBeCalledWith(StatusCodes.BAD_REQUEST)
+      const jsonFn = jest.spyOn(res, 'json')
+      expect(jsonFn.mock.calls[0][0].error).toMatch(
+        `Creating request with streamId ${req.body.streamId} and commit CID ${req.body.cid} failed`
+      )
+    })
+
+    test('create request', async () => {
+      const cid = randomCID()
+      const streamId = randomStreamID()
+      const timestamp = new Date()
+      const origin = '203.0.113.195'
+      const req = mockRequest({
+        headers: {
+          'X-Forwarded-For': [` ${origin}`, `${origin}, 2001:db8:85a3:8d3:1319:8a2e:370:7348`],
+        },
+        body: {
+          cid: cid.toString(),
+          streamId: streamId.toString(),
+          timestamp: timestamp.toISOString(),
+        },
+      })
+      const res = mockResponse()
+      const requestRepository = container.resolve('requestRepository')
+      await expect(requestRepository.findByCid(cid)).resolves.toBeUndefined()
+      const now = new Date()
+      await controller.createRequest(req, res)
+      expect(res.status).toBeCalledWith(StatusCodes.CREATED)
+      const createdRequest = await requestRepository.findByCid(cid)
+      expect(createdRequest).toBeDefined()
+      expect(createdRequest.cid).toEqual(cid.toString())
+      expect(createdRequest.status).toEqual(RequestStatus.PENDING)
+      expect(createdRequest.streamId).toEqual(streamId.toString())
+      expect(createdRequest.message).toEqual('Request is pending.')
+      expect(createdRequest.timestamp.valueOf()).toEqual(timestamp.valueOf())
+      expect(createdRequest.createdAt.valueOf()).toBeCloseTo(now.valueOf(), -1.4) // within ~12 ms
+      expect(createdRequest.updatedAt.valueOf()).toBeCloseTo(now.valueOf(), -1.4) // within ~12 ms
+      expect(createdRequest.origin).toEqual(origin)
+    })
+
+    test('timestamp is empty', async () => {
+      const cid = randomCID()
+      const streamId = randomStreamID()
+      const now = new Date()
+      const req = mockRequest({
+        body: {
+          cid: cid.toString(),
+          streamId: streamId.toString(),
+        },
+      })
+      const res = mockResponse()
+      const requestRepository = container.resolve('requestRepository')
+      await expect(requestRepository.findByCid(cid)).resolves.toBeUndefined()
+      await controller.createRequest(req, res)
+      expect(res.status).toBeCalledWith(StatusCodes.CREATED)
+      const createdRequest = await requestRepository.findByCid(cid)
+      expect(createdRequest).toBeDefined()
+      expect(createdRequest.cid).toEqual(cid.toString())
+      expect(createdRequest.status).toEqual(RequestStatus.PENDING)
+      expect(createdRequest.streamId).toEqual(streamId.toString())
+      expect(createdRequest.message).toEqual('Request is pending.')
+      expect(createdRequest.timestamp.valueOf()).toBeCloseTo(now.valueOf(), -1.4) // within ~12 ms
+      expect(createdRequest.createdAt.valueOf()).toBeCloseTo(now.valueOf(), -1.4) // within ~12 ms
+      expect(createdRequest.updatedAt.valueOf()).toBeCloseTo(now.valueOf(), -1.4) // within ~12 ms
+      expect(createdRequest.origin).not.toBeNull()
+    })
+
+    test('fill metadata', async () => {
+      const cid = randomCID()
+      const streamId = randomStreamID()
+      const req = mockRequest({
+        body: {
+          cid: cid.toString(),
+          streamId: streamId.toString(),
+        },
+      })
+      const metadataService = container.resolve('metadataService')
+      const fillSpy = jest.spyOn(metadataService, 'fill')
+      await controller.createRequest(req, mockResponse())
+      expect(fillSpy).toBeCalledWith(streamId)
+    })
+  })
+
+  describe('existing request', () => {
+    test('return representation', async () => {
+      // 0. Prepare
+      const cid = randomCID()
+      const streamId = randomStreamID()
+      const now = new Date()
+      const req = mockRequest({
+        body: {
+          cid: cid.toString(),
+          streamId: streamId.toString(),
+          timestamp: now.toISOString(),
+        },
+      })
+      const res = mockResponse()
+      await controller.createRequest(req, res)
+      const jsonFn = jest.spyOn(res, 'json')
+      const presentation0 = jsonFn.mock.lastCall[0]
+
+      // 1. Request existing request
+      const res1 = mockResponse()
+      await controller.createRequest(req, res1)
+      const jsonFn1 = jest.spyOn(res1, 'json')
+      const presentation1 = jsonFn1.mock.lastCall[0]
+      expect(presentation1).toEqual(presentation0)
+    })
+  })
+})
--- src/controllers/request-controller.ts
+++ src/controllers/request-controller.ts
@@ -7,14 +7,38 @@ import cors from 'cors'
 import { ClassMiddleware, Controller, Get, Post } from '@overnightjs/core'
 
 import { toCID } from '@ceramicnetwork/common'
+import { StreamID } from '@ceramicnetwork/streamid'
 import { AnchorRepository } from '../repositories/anchor-repository.js'
-import { RequestRepository } from '../repositories/request-repository.js'
 import { Request, RequestStatus } from '../models/request.js'
 import { logger } from '../logger/index.js'
 import { ServiceMetrics as Metrics } from '@ceramicnetwork/observability'
 import { METRIC_NAMES } from '../settings.js'
 import { CeramicService } from '../services/ceramic-service.js'
 import type { IRequestPresentationService } from '../services/request-presentation-service.type.js'
+import type { RequestRepository } from '../repositories/request-repository.js'
+import type { IIpfsService } from '../services/ipfs-service.type.js'
+import type { IMetadataService } from '../services/metadata-service.js'
+
+/*
+ * Get origin from a request from X-Forwarded-For.
+ * Parsing according to https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-For#parsing
+ *
+ * If no header found, use IP address of the requester.
+ *
+ * TODO CDB-2185 Get it from DID signer first.
+ */
+function parseOrigin(req: ExpReq): string {
+  let addresses = req.ip
+  const xForwardedForHeader = req.get('X-Forwarded-For')
+  if (xForwardedForHeader) {
+    if (Array.isArray(xForwardedForHeader)) {
+      addresses = xForwardedForHeader.join(',')
+    } else {
+      addresses = xForwardedForHeader
+    }
+  }
+  return addresses.split(',')[0].trim()
+}
 
 @Controller('api/v0/requests')
 @ClassMiddleware([cors()])
@@ -25,14 +49,18 @@ export class RequestController {
     'requestRepository',
     'ceramicService',
     'requestPresentationService',
+    'ipfsService',
+    'metadataService',
   ] as const
 
   constructor(
-    private config: Config,
-    private anchorRepository: AnchorRepository,
-    private requestRepository: RequestRepository,
-    private ceramicService: CeramicService,
-    private readonly requestPresentationService: IRequestPresentationService
+    private readonly config: Config,
+    private readonly anchorRepository: AnchorRepository,
+    private readonly requestRepository: RequestRepository,
+    private readonly ceramicService: CeramicService,
+    private readonly requestPresentationService: IRequestPresentationService,
+    private readonly ipfsService: IIpfsService,
+    private readonly metadataService: IMetadataService
   ) {}
 
   @Get(':cid')
@@ -41,21 +69,20 @@ export class RequestController {
 
     try {
       const cid = toCID(req.params.cid)
-      if (cid) {
-        const request = await this.requestRepository.findByCid(cid)
-        if (request) {
-          const body = await this.requestPresentationService.body(request)
-          return res.status(StatusCodes.OK).json(body)
-        } else {
-          return res.status(StatusCodes.OK).send({
-            error: "Request doesn't exist",
-          })
-        }
-      } else {
-        return res.status(StatusCodes.BAD_REQUEST).send({
+      if (!cid) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
           error: 'CID is empty',
         })
       }
+      const request = await this.requestRepository.findByCid(cid)
+      if (!request) {
+        return res.status(StatusCodes.OK).json({
+          error: "Request doesn't exist",
+        })
+      }
+
+      const body = await this.requestPresentationService.body(request)
+      return res.status(StatusCodes.OK).json(body)
     } catch (err) {
       const errmsg = `Loading request status for CID ${req.params.cid} failed: ${err.message}`
       logger.err(errmsg)
@@ -66,53 +93,60 @@ export class RequestController {
   }
 
   @Post()
-  private async createRequest(req: ExpReq, res: ExpRes): Promise<ExpRes<any>> {
+  async createRequest(req: ExpReq, res: ExpRes): Promise<ExpRes<any>> {
     try {
       logger.debug(`Create request ${JSON.stringify(req.body)}`)
 
-      const cid = req.body.cid
-      // TODO docId check for backwards compat with old Ceramic nodes. Remove once
-      //  https://github.com/ceramicnetwork/js-ceramic/issues/1485 has been merged, released, and
-      // deployed to a majority of the network.
-      const streamId = req.body.streamId || req.body.docId
-
-      if (cid == null) {
-        return res.status(StatusCodes.BAD_REQUEST).send({
+      const cidInput = req.body.cid
+      if (!cidInput) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
           error: 'CID is empty',
         })
       }
+      const cid = toCID(cidInput)
 
-      if (streamId == null) {
-        return res.status(StatusCodes.BAD_REQUEST).send({
+      if (!req.body.streamId) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
           error: 'Stream ID is empty',
         })
       }
+      const streamId = StreamID.fromString(req.body.streamId)
 
-      const cidObj = toCID(cid)
-      let request = await this.requestRepository.findByCid(cidObj)
-      if (request) {
-        const body = await this.requestPresentationService.body(request)
-        return res.status(StatusCodes.ACCEPTED).json(body)
-      } else {
-        // Intentionally don't await the pinStream promise, let it happen in the background.
-        this.ceramicService.pinStream(streamId)
-        Metrics.count(METRIC_NAMES.ANCHOR_REQUESTED, 1, { ip_addr: req.ip })
+      // Store metadata from genesis to the database
+      // TODO CDB-2151 This should be moved out of RequestController
+      await this.metadataService.fill(streamId)
 
-        request = new Request()
-        request.cid = cid.toString()
-        request.streamId = streamId
-        request.status = RequestStatus.PENDING
-        request.message = 'Request is pending.'
-        // We don't actually know with certainty that the stream is pinned, since the pinStream
-        // call above can fail and swallows errors, but marking it as pinned incorrectly is harmless,
-        // and this way we ensure the request is picked up by garbage collection.
-        request.pinned = true
-
-        request = await this.requestRepository.createOrUpdate(request)
-
-        const body = await this.requestPresentationService.body(request)
-        return res.status(StatusCodes.CREATED).json(body)
+      let timestamp = new Date()
+      if (req.body.timestamp) {
+        timestamp = new Date(req.body.timestamp)
       }
+
+      const found = await this.requestRepository.findByCid(cid)
+      if (found) {
+        const body = await this.requestPresentationService.body(found)
+        return res.status(StatusCodes.ACCEPTED).json(body)
+      }
+
+      // Intentionally don't await the pinStream promise, let it happen in the background.
+      this.ceramicService.pinStream(streamId)
+      Metrics.count(METRIC_NAMES.ANCHOR_REQUESTED, 1, { ip_addr: req.ip })
+
+      const request = new Request()
+      request.cid = cid.toString()
+      request.origin = parseOrigin(req)
+      request.streamId = streamId.toString()
+      request.status = RequestStatus.PENDING
+      request.message = 'Request is pending.'
+      // We don't actually know with certainty that the stream is pinned, since the pinStream
+      // call above can fail and swallows errors, but marking it as pinned incorrectly is harmless,
+      // and this way we ensure the request is picked up by garbage collection.
+      request.pinned = true
+      request.timestamp = timestamp
+
+      const storedRequest = await this.requestRepository.createOrUpdate(request)
+
+      const body = await this.requestPresentationService.body(storedRequest)
+      return res.status(StatusCodes.CREATED).json(body)
     } catch (err) {
       const errmsg = `Creating request with streamId ${req.body.streamId} and commit CID ${req.body.cid} failed: ${err.message}`
       logger.err(errmsg)
