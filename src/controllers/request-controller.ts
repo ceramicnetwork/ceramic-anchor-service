2a6d947a5cae63c8a3510ab8c542f4ff217aadcf
Sergey Ukustov <sergey@ukstv.me>
2023-02-15 07:38:15 +0300

    chore: Stricter types in request-controller.ts

--- src/controllers/request-controller.ts
+++ src/controllers/request-controller.ts
@@ -4,7 +4,7 @@ import { Request as ExpReq, Response as ExpRes } from 'express'
 import cors from 'cors'
 import {ClassMiddleware, Controller, Get, Middleware, Post} from '@overnightjs/core'
 
-import { toCID } from '@ceramicnetwork/common'
+import { NonEmptyArray, toCID } from '@ceramicnetwork/common'
 import { Request, RequestStatus } from '../models/request.js'
 import { logger } from '../logger/index.js'
 import { ServiceMetrics as Metrics } from '@ceramicnetwork/observability'
@@ -40,7 +40,8 @@ function parseOrigin(req: ExpReq): string {
       addresses = xForwardedForHeader
     }
   }
-  return addresses.split(',')[0].trim()
+  const addressesSplit = addresses.split(',') as NonEmptyArray<string>
+  return addressesSplit[0].trim()
 }
 
 @Controller('api/v0/requests')
@@ -61,11 +62,11 @@ export class RequestController {
   ) {}
 
   @Get(':cid')
-  private async getStatusForCid(req: ExpReq, res: ExpRes): Promise<ExpRes<any>> {
-    logger.debug(`Get info for ${req.params.cid}`)
+  async getStatusForCid(req: ExpReq, res: ExpRes): Promise<ExpRes<any>> {
+    logger.debug(`Get info for ${req.params['cid']}`)
 
     try {
-      const cid = toCID(req.params.cid)
+      const cid = toCID(req.params['cid'] as string)
       if (!cid) {
         return res.status(StatusCodes.BAD_REQUEST).json({
           error: 'CID is empty',
@@ -80,8 +81,8 @@ export class RequestController {
 
       const body = await this.requestPresentationService.body(request)
       return res.status(StatusCodes.OK).json(body)
-    } catch (err) {
-      const errmsg = `Loading request status for CID ${req.params.cid} failed: ${err.message}`
+    } catch (err: any) {
+      const errmsg = `Loading request status for CID ${req.params['cid']} failed: ${err.message}`
       logger.err(errmsg)
       return res.status(StatusCodes.BAD_REQUEST).json({
         error: errmsg,
@@ -151,7 +152,7 @@ export class RequestController {
 
       const body = await this.requestPresentationService.body(storedRequest)
       return res.status(StatusCodes.CREATED).json(body)
-    } catch (err) {
+    } catch (err: any) {
       return this.getBadRequestResponse(req, res, err)
     }
   }
