d06e0edca4aabbc1644b2e1637748d394a29cebc
Sergey Ukustov <sergey@ukstv.me>
2023-02-15 10:26:09 +0300

    chore: No node-specific modules in merkle-proof test

--- src/merkle/__tests__/merkle-proof.test.ts
+++ src/merkle/__tests__/merkle-proof.test.ts
@@ -1,53 +1,38 @@
 import { describe, test, expect, beforeAll } from '@jest/globals'
-import * as crypto from 'crypto'
 import { MergeFunction, Node } from '../merkle.js'
 import type { MerkleTree } from '../merkle-tree.js'
 import { MerkleTreeFactory } from '../merkle-tree-factory.js'
 import { StringConcat } from './string-concat.js'
 import { expectPresent } from '../../__tests__/expect-present.util.js'
+import * as uint8arrays from 'uint8arrays'
+import { hash as sha256 } from '@stablelib/sha256'
 
-// use the crypto module to create a sha256 hash from the node passed in
-const sha256 = (data: any): Uint8Array => {
-  return crypto.createHash('sha256').update(data).digest()
-}
-
-// tslint:disable-next-line:max-classes-per-file
 class HashConcat implements MergeFunction<Uint8Array, Uint8Array> {
   async merge(
     n1: Node<Uint8Array>,
     n2: Node<Uint8Array>,
     m: Uint8Array | null
   ): Promise<Node<Uint8Array>> {
-    if (!n1) {
-      throw new Error('The concat function expects two hash arguments, the first was not received.')
-    }
-    if (!n2) {
-      throw new Error(
-        'The concat function expects two hash arguments, the second was not received.'
-      )
-    }
     const elems = [n1.data, n2.data]
-    if (m) {
-      elems.push(m)
-    }
-    return new Node(sha256(Buffer.concat(elems)), n1, n2)
+    if (m) elems.push(m)
+    return new Node(sha256(uint8arrays.concat(elems)), n1, n2)
   }
 }
 
 // given a proof, finds the merkle root
-const hashProof = (value: any, proof: Node<Uint8Array>[]): any => {
-  let data: Uint8Array = sha256(value)
+function hashProof(value: string, proof: Node<Uint8Array>[]): Uint8Array {
+  let data = sha256(uint8arrays.fromString(value))
   for (let i = 0; i < proof.length; i++) {
     let buffers: Uint8Array[]
     const element = proof[i]
     expectPresent(element)
     const left = element.parent?.left === proof[i]
     if (left) {
-      buffers = new Array<Uint8Array>(element.data, data)
+      buffers = [element.data, data]
     } else {
-      buffers = new Array<Uint8Array>(data, element.data)
+      buffers = [data, element.data]
     }
-    data = sha256(Buffer.concat(buffers))
+    data = sha256(uint8arrays.concat(buffers))
   }
   return data
 }
@@ -63,7 +48,9 @@ const lettersTreeFactory = new MerkleTreeFactory<string, string, string>(new Str
 
 describe('Merkle tree proofs tests', () => {
   beforeAll(async () => {
-    hashTree = await hashTreeFactory.build(leaves.map(sha256))
+    hashTree = await hashTreeFactory.build(
+      leaves.map((leaf) => sha256(uint8arrays.fromString(leaf)))
+    )
     lettersTree = await lettersTreeFactory.build(leaves)
   })
 
@@ -71,10 +58,9 @@ describe('Merkle tree proofs tests', () => {
     test.each(leaves)(`should return a proof that calculates the root from leaf %p`, (leaf) => {
       const i = leaves.indexOf(leaf)
       const proof = hashTree.getProof(i)
-      const hashedProof = hashProof(leaf, proof).toString('hex')
+      const hashedProof = uint8arrays.toString(hashProof(leaf, proof), 'hex')
       if (hashedProof !== root) {
         const lettersProof = lettersTree.getProof(i)
-        // tslint:disable-next-line:no-console
         console.log(
           'The resulting hash of your proof is wrong. \n' +
             `We were expecting: ${root} \n` +
@@ -84,7 +70,7 @@ describe('Merkle tree proofs tests', () => {
         )
       }
 
-      expect(hashedProof).toStrictEqual(root)
+      expect(hashedProof).toEqual(root)
     })
   })
 })
