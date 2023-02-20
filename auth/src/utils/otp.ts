0062bb7e01de250363083dc064beb068fad06b5e
v-stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2023-02-20 13:00:04 -0500

    fix: use uuid v4

--- auth/src/utils/otp.ts
+++ auth/src/utils/otp.ts
@@ -1,6 +1,6 @@
-import { uuid } from '@stablelib/uuid'
+import * as uuid from '@stablelib/uuid'
 
 export const generateOTP = () => {
     if (process.env.TESTING == 'true') return '29161b43-758a-40f3-aece-97758bac617a'
-    return uuid()
+    return uuid.v4()
 }
