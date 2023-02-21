92b237b37f403022e5a1c989d61f887aaebd7eb2
v-stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2023-02-21 13:27:40 -0500

    fix: replace stablelib uuid with crypto

--- auth/src/utils/otp.ts
+++ auth/src/utils/otp.ts
@@ -1,6 +1,6 @@
-import * as uuid from '@stablelib/uuid'
+import { randomUUID } from 'crypto'
 
 export const generateOTP = () => {
     if (process.env.TESTING == 'true') return '29161b43-758a-40f3-aece-97758bac617a'
-    return uuid.uuid()
+    return randomUUID()
 }
