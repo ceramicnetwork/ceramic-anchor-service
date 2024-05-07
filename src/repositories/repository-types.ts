f09fb3d8f23e82b41824897ea707d5d3a6c45ab3
stephhuynh18 <stephhuynh18@users.noreply.github.com>
2024-05-07 11:01:14 -0700

    Fix: retry db transaction if we get a serialization error when persisting anchors

--- src/repositories/repository-types.ts
+++ src/repositories/repository-types.ts
@@ -1,9 +1,11 @@
 import type { Knex } from 'knex'
 
+// application is recommended to automatically retry when seeing this error
+export const REPEATED_READ_SERIALIZATION_ERROR = '40001'
+
 /**
  * Options for repository functions
  */
-
 export interface Options {
   connection?: Knex // connection to use in query (allows for transactions)
 }
